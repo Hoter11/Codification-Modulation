e0e40ccca6f7b2ac033a2ad173d8b0a84d771b7c
roc <granadaroc96@gmail.com>
2016-10-17 20:58:53 +0200

    First commit, initiation of the program

---
+++ RocGranada_Cod_Mod.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Author: Roc Granada Verdú
+# RocGranada_Cod_Mod.py
+
+"""
+Given a sequence of bits and a choosen codification/modulation model
+apply it to the previous sequence and return the result.
+"""
+
+#Dictionary that has all the models names and an id associated
+models = {}
+
+class model:
+    """Class that englovates the information necessary for the codification/modulation"""
+    def __init__(self):
+        self.model
+        self.sequence
+
+def main(sequence):
+    return 0
+
+print "Choosen model: ", self.model
+print "Sequence at begining: ", self.sequence
+print "Sequence result: ", main(self.sequence)
