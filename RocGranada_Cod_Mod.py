179746a191b82fa2d24d410c1cf20abe9b56f1b0
roc <granadaroc96@gmail.com>
2016-11-12 18:24:35 +0100

    Done until Pseudoternary

--- RocGranada_Cod_Mod.py
+++ RocGranada_Cod_Mod.py
@@ -2,31 +2,43 @@
 
 # Author: Roc Granada Verdú
 # RocGranada_Cod_Mod.py
+import random
+import matplotlib.pyplot as plt
 
 """
 Given a sequence of bits and a choosen codification/modulation model
 apply it to the previous sequence and return the result.
 """
-SEQUENCE = 0x01001110101
+SEQUENCE = "01001110101"
 
 #Prints for the menu
 header = "\n*** Welcome, choose the option you prefere ***"
-options = """\t1. Nonreturn to Zero-Level (NRZ-L)
-\t2. Nonreturn to Zero Inverted (NRZI)
-\t3. Bipolar - AMI
-\t4. Pseudoternary
-\t5. Manchester
-\t6. Differential Manchester
-\t7. B8ZS
-\t8. HDB3
+options = """\t1. Nonreturn to Zero (NRZ)
+\t2. Nonreturn to Zero-Level (NRZ-L)
+\t3. Nonreturn to Zero Inverted (NRZI)
+\t4. Bipolar - AMI
+\t5. Pseudoternary
+\t6. Manchester
+\t7. Differential Manchester
+\t8. B8ZS
+\t9. HDB3
 """
-input_sequence = "Enter your sequence: (blank for predefined one)"
+input_sequence = "Enter your sequence: (blank for random one)"
 
 class Model:
     """Class that englovates the information necessary for the codification/modulation"""
     def __init__(self):
         self.option = 1
-        self.sequence = 1
+        self.sequence = [0,1,0,1]
+
+    def __str__(self):
+        #seq = [int(elem) for elem in self.sequence]
+        plt.plot(self.sequence)
+        plt.axis([0,len(self.sequence),min(self.sequence)-1,2])
+        plt.xlabel("Bits")
+        plt.ylabel("Levels")
+        plt.show()
+        return ""
 
     # Getters & Setters
     def setOption(self,option):
@@ -45,24 +57,61 @@ def askData(model):
     opt_in = input()
     model.setOption(opt_in)
     print input_sequence
-    try:
-        seq_in = input()
-        model.setSequence(int(seq_in,2))
-    except SyntaxError:
+    seq_in = raw_input()
+    if seq_in != "":
+        model.setSequence(seq_in)
+    else:
+        SEQUENCE = ""
+        for i in range(50): SEQUENCE += str(random.randint(0,1))
         model.setSequence(SEQUENCE)
 
 #1:NRZ-L,2:NRZI,3:AMI,4:Pseudoternary,5:Manchester,6:Diff_manchester,7:B8ZS,8:HDB3
-def NRZ_L(seq):
-    return seq
 
-def NRZI(seq):
-    return seq
+def NRZ(model):
+    """Nothing to do to the sequence"""
+    model.setSequence([int(elem) for elem in model.getSequence()])
+    pass
+
+def NRZ_L(model):
+    """Invert 1's and 0's levels"""
+    dic = {'1':0,'0':1}
+    model.setSequence([dic[elem] for elem in model.getSequence()])
+
+def NRZI(model):
+    """Change level when 1 found"""
+    dic = {1:0,0:1}
+    seq = [0]
+    for elem in model.getSequence():
+        if elem == '1':
+            seq.append(dic[seq[-1]])
+        else:
+            seq.append(seq[-1])
+    seq.remove(seq[0])
+    model.setSequence(seq)
 
 def AMI(seq):
-    return seq
+    """0 always on 0, 1 changes from 1 to -1"""
+    last = -1
+    seq = []
+    for elem in model.getSequence():
+        if elem == '1':
+            last = last*(-1)
+            seq.append(last)
+        else:
+            seq.append(0)
+    model.setSequence(seq)
 
 def Pseudoternary(seq):
-    return seq
+    """1 always on 0, 0 changes from 1 to -1"""
+    last = -1
+    seq = []
+    for elem in model.getSequence():
+        if elem == '0':
+            last = last*(-1)
+            seq.append(last)
+        else:
+            seq.append(0)
+    model.setSequence(seq)
 
 def Manchester(seq):
     return seq
@@ -77,15 +126,19 @@ def HDB3(seq):
     return seq
 
 #Dictionary that has all the models names and an id associated
-models = {1:NRZ_L,2:NRZI,3:AMI,4:Pseudoternary,5:Manchester,6:Diff_manchester,7:B8ZS,8:HDB3}
+models = {1:NRZ,2:NRZ_L,3:NRZI,4:AMI,5:Pseudoternary,6:Manchester,7:Diff_manchester,8:B8ZS,9:HDB3}
 
 def main(model):
     """Method that codifies/modulates the sequence in the given option"""
     func = models[model.getOption()]
-    return func(model.getSequence())
+    func(model)
+    seq = ""
+    for elem in model.getSequence(): seq += str(elem)
+    return seq
 
 model = Model()
 askData(model)
 print "Choosen model: ", model.option
 print "Sequence at begining: ", model.sequence
 print "Sequence result: ", main(model)
+print model
